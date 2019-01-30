787fb6ecd4323a875e34be1a12bb0bff66acd8e8
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-01-30 19:08:32 +0100

    next

--- src/aidbox/code_generator.clj
+++ src/aidbox/code_generator.clj
@@ -5,10 +5,16 @@
 
 
 (defn string->keyword [s]
-  (-> s
-      string/lower-case
-      (string/replace #"[_\s/]+" "-")
-      keyword))
+  (or
+   (some-> s
+           string/lower-case
+           (string/replace #"[(-.^].+" "")
+           (string/replace #"&.+" "")
+           (string/replace #"^\d+" "")
+           string/trim
+           (string/replace #"[_\s/]+" "-")
+           keyword)
+   :invalid-string))
 
 (defn parse-one-segment [s]
   (let [[header _ & fields] (->> s
@@ -16,31 +22,45 @@
                                  (map string/trim)
                                  (remove string/blank?))
         [_ info id]         (re-find #"^(.+?)?\s*\((.+)\).*$" header)]
-    {:info   info
-     :id     id
-     :fields (->> fields
-                  (map #(string/split % #"\t+"))
-                  (map (fn [[sequence	length data-type required repetition name]]
-                         (let [id (string->keyword name)]
-                           {:id         id
-                            :sequence   (read-string sequence)
-                            :length     (read-string  length)
-                            :data-type  (string->keyword data-type)
-                            :required   (= required "REQ")
-                            :repetition (string->keyword repetition)
-                            :info       name}))))}))
+    (when id
+     (println ">>" info id)
+     {:info   info
+      :id     id
+      :fields (->> fields
+                   (map #(string/split % #"\t+"))
+                   (map (fn [[sequence	length data-type required repetition name]]
+                          (let [id (string->keyword name)]
+                            {:id         id
+                             :sequence   (read-string sequence)
+                             :length     (read-string  length)
+                             :data-type  (string->keyword data-type)
+                             :required   (= required "REQ")
+                             :repetition (string->keyword repetition)
+                             :info       name}))))})))
 
-(defn create-method [segment]
-  ";; segment")
+(defn- wrap-vector [s]
+  (str "[" s "]"))
+
+(defn create-method [{:keys [info id fields]}]
+  (let [vars-str (->> fields
+                      (map (fn [x]
+                             (-> x :id name wrap-vector)))
+                      (string/join "\n         "))
+        map-str  ""]
+    (format
+     (slurp (io/resource "templates/segment-method.txt"))
+      info id vars-str id map-str)))
 
 (defn generate-orm []
   (->> (io/resource "tables/segments")
        io/as-file
        file-seq
        (sort-by #(-> % .lastModified))
-       (take 1)
+       (remove #(-> % .isDirectory))
+       ;; (take 1)
        (map slurp)
        (map parse-one-segment)
+       (remove nil?)
        (map create-method)
        (string/join "\n\n")
        (format (slurp (io/resource "templates/orm.txt")))
@@ -48,7 +68,7 @@
 
 (comment
 
-  (pprint (generate-orm))
+  (generate-orm)
 
 
   )
